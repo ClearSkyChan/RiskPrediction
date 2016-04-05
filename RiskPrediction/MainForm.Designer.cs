e26c16729422acb543d85a98f96cdc0ee29af500
clearskychan <clearskychan@qq.com>
2016-04-05 15:14:15 +0800

    first

---
+++ RiskPrediction/MainForm.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace RiskPrediction
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataImport = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.fileNameLbl = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataImport
+            // 
+            this.dataImport.Location = new System.Drawing.Point(119, 22);
+            this.dataImport.Name = "dataImport";
+            this.dataImport.Size = new System.Drawing.Size(75, 23);
+            this.dataImport.TabIndex = 0;
+            this.dataImport.Text = "导入";
+            this.dataImport.UseVisualStyleBackColor = true;
+            this.dataImport.Click += new System.EventHandler(this.dataImport_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // fileNameLbl
+            // 
+            this.fileNameLbl.AutoSize = true;
+            this.fileNameLbl.Location = new System.Drawing.Point(15, 27);
+            this.fileNameLbl.Name = "fileNameLbl";
+            this.fileNameLbl.Size = new System.Drawing.Size(95, 12);
+            this.fileNameLbl.TabIndex = 1;
+            this.fileNameLbl.Text = "选择输入文件...";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.fileNameLbl);
+            this.groupBox1.Controls.Add(this.dataImport);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(221, 296);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "样本";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Location = new System.Drawing.Point(253, 112);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(212, 196);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "计算";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "选择输出文件...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "导入";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(61, 212);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "学习";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(60, 112);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "计算";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "选择输入文件...";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(131, 41);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "导入";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "最大绝对错误";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(119, 163);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 21);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "最大迭代次数";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Location = new System.Drawing.Point(253, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(212, 94);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "网络";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(17, 38);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "导入";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(60, 112);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "计算";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(115, 38);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "导出";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 326);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.Text = "BP神经网络Demo";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button dataImport;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label fileNameLbl;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ RiskPrediction/MainForm.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AForge.Neuro;
+using AForge.Neuro.Learning;
+
+namespace RiskPrediction
+{
+    public partial class MainForm : Form
+    {
+        private int InputLength { get; set; }
+        private int SampleCount { get; set; }
+        private int OutputLength { get; set; }
+        public int OutputIndex { get; set; }
+        public int InputIndex { get; set; }
+        double[][] Input{get; set; }
+        double[][] Output { get; set; }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void dataImport_Click(object sender, EventArgs e)
+        {
+            Import();
+            PremnmxInput();
+
+            var netWork = new ActivationNetwork(new SigmoidFunction(2), InputLength,6,OutputLength);
+            var teacher = new BackPropagationLearning(netWork);
+            
+            // 定义绝对误差 
+            double error = 1.0;
+
+            teacher.LearningRate = 0.1;
+
+            // 迭代次数 
+            int iterations = 0;
+
+            while (error >= 0.001 && iterations < 100000)
+            {   
+                error = teacher.RunEpoch(Input, Output) ;
+                iterations++;
+            }
+
+            var result = new double[SampleCount][];
+
+            for (var i = 0; i < SampleCount; i++)
+            {
+                result[i] = netWork.Compute(Input[i]);
+                for (var j = 0; j < result.Length; j++)
+                {
+                    //var bytes = System.Text.Encoding.Default.GetBytes(result[j] + ",");
+                    //writer.Write(bytes,0,bytes.Length);
+                }
+                //writer.Write(System.Text.Encoding.Default.GetBytes("\r\n"), 0, 2);
+            }
+
+
+        }
+
+        private void Import()
+        {
+            InputIndex = 0;
+            OutputIndex = 65;
+
+            InputLength = 65;
+            OutputLength = 4;
+
+            SampleCount = 7;
+            Input = new double[SampleCount][];
+            Output = new double[SampleCount][];
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileNameLbl.Text = openFileDialog.FileName;
+                using (var reader = File.OpenText(openFileDialog.FileName))
+                {
+                    string line;
+                    int lineCount = 0;
+
+                    reader.ReadLine(); //表头跳过
+
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        var datas = line.Split(';');
+                        if (datas.Length == 1)
+                            datas = line.Split(',');
+
+                        Input[lineCount] = new double[InputLength];
+                        for (var i = InputIndex; i < InputIndex+InputLength; i++)
+                        {
+                            Input[lineCount][i - InputIndex] = double.Parse(datas[i]);
+                        }
+                        Output[lineCount] = new double[OutputLength];
+                        for (var i = OutputIndex; i < OutputIndex + OutputLength; i++)
+                        {
+                            Output[lineCount][i - OutputIndex] = double.Parse(datas[i]);
+                        }
+                        lineCount++;
+                        ;
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+        private static double Premnmx(double num, double min, double max)
+        {
+            if (num > max)
+                num = max;
+            if (num < min)
+                num = min;
+
+            return (num - min) / (max - min);
+        }
+
+        private void PremnmxInput()
+        {
+
+            var inputMax = new double[SampleCount];
+            var inputMin = new double[SampleCount];
+            for (var i = 0; i < SampleCount; i++)
+            {
+                var min = double.MaxValue;
+                var max = double.MinValue;
+                for (var j = 0; j < InputLength; j++)
+                {
+                    if (min > Input[i][j])
+                    {
+                        min = Input[i][j];
+                    }
+                    if (max < Input[i][j])
+                    {
+                        max = Input[i][j];
+                    }
+                }
+                inputMax[i] = max;
+                inputMin[i] = min;
+            }
+
+            for (var i = 0; i < SampleCount; i++)
+            {
+                var min = inputMin[i];
+                var max = inputMax[i];
+                for (var j = 0; j < InputLength; j++)
+                {
+                    Input[i][j] = Premnmx(Input[i][j], min, max);                 
+                }
+                for (var j = 0; j < OutputLength; j++)
+                {
+                    Output[i][j] = Output[i][j]/10;
+                }
+            }
+        }
+
+
+        private void Train(double[][] input, double[][] output)
+        {
+            var netWork = new ActivationNetwork(new SigmoidFunction(), InputLength, 8, 4);
+            var teacher = new PerceptronLearning(netWork);
+
+            // 定义绝对误差 
+            double error = 1.0;
+
+            // 迭代次数 
+            int iterations = 0;
+
+            while (error > 0.001 && iterations< 5000)
+            {
+                error = teacher.RunEpoch(input, output);                
+                iterations++;
+            }
+        }
+    }
+}
